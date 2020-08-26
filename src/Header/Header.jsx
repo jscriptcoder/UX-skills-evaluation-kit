e8ee95d332c0621da4c5802d18a8051b3dde89f0
Francisco Ramos <jscriptcoder@gmail.com>
2020-08-26 12:06:56 +0200

    WIP: header user

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,23 @@
+import './Header.less'
+
 import React, { useContext } from 'react'
 import { Layout } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 
 import { AppContext } from '../Store'
 
 export default function Header() {
   const { state } = useContext(AppContext)
+  const { name } = state.user
 
   return (
     <Layout.Header className="Header">
-      <img src="user-wizar-logo.png" style={{ height: 32 }} />
+      <div className="Header__logo">
+        <img src="user-wizar-logo.png" style={{ height: 32 }} />
+      </div>
+      <div className="Header__user">
+        <UserOutlined /> {name}
+      </div>
     </Layout.Header>
   )
 }
